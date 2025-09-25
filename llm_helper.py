0807e0c4bf498487fc0f24256e2634da2f653654
Bakhtawar23 <bains.bakhtawar2328@gmail.com>
2025-09-26 00:56:01 +0530

    LinkedinGenAI

--- llm_helper.py
+++ llm_helper.py
@@ -1,22 +1,11 @@
-import os
-import streamlit as st
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
 
-# Load from .env locally
 load_dotenv()
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"), model_name="meta-llama/llama-4-maverick-17b-128e-instruct")
 
-# First try Streamlit secrets, then fallback to environment variable
-groq_api_key = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
-
-if not groq_api_key:
-    raise ValueError("❌ GROQ_API_KEY not found. Please set it in Streamlit Secrets or .env file.")
-
-llm = ChatGroq(
-    groq_api_key=groq_api_key,
-    model_name="meta-llama/llama-4-maverick-17b-128e-instruct"
-)
 
 if __name__ == "__main__":
-    response = llm.invoke("Last winner of Asia Cup t20 format")
+    response = llm.invoke("Two most important ingradient in samosa are ")
     print(response.content)
